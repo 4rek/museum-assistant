28059d08c5f3b781e3876ded8b824e6ad06c1538
A. Juszczyk <arkadiusz.juszczyk@edu.uekat.pl>
2025-08-20 23:17:59 +0200

    feat: added supabase auth integration

---
+++ lib/supabase.ts
@@ -0,0 +1,43 @@
+import "react-native-url-polyfill/auto";
+import { AppState } from "react-native";
+import * as SecureStore from "expo-secure-store";
+import { createClient, processLock } from "@supabase/supabase-js";
+
+const supabaseUrl = "https://czbvosthargmigcymosy.supabase.co";
+const supabaseAnonKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImN6YnZvc3RoYXJnbWlnY3ltb3N5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTU3MjA4NDQsImV4cCI6MjA3MTI5Njg0NH0.jYjHJPhSDwEuTG1mRrBQPq0WpDbzv1Kgtc4mo4L9xuE";
+
+const ExpoSecureStorageAdapter = {
+  getItem: (key: string) => {
+    return SecureStore.getItemAsync(key);
+  },
+  setItem: (key: string, value: string) => {
+    return SecureStore.setItemAsync(key, value);
+  },
+  removeItem: (key: string) => {
+    return SecureStore.deleteItemAsync(key);
+  },
+};
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
+  auth: {
+    storage: ExpoSecureStorageAdapter,
+    autoRefreshToken: true,
+    persistSession: true,
+    detectSessionInUrl: false,
+    lock: processLock,
+  },
+});
+
+// Tells Supabase Auth to continuously refresh the session automatically
+// if the app is in the foreground. When this is added, you will continue
+// to receive `onAuthStateChange` events with the `TOKEN_REFRESHED` or
+// `SIGNED_OUT` event if the user's session is terminated. This should
+// only be registered once.
+AppState.addEventListener("change", (state) => {
+  if (state === "active") {
+    supabase.auth.startAutoRefresh();
+  } else {
+    supabase.auth.stopAutoRefresh();
+  }
+});
